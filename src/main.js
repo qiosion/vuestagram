1d86208ec2767cb84c6d55aef6310d2d78853a96
qiosion <qiosion@gmail.com>
2024-06-12 10:32:45 +0900

    mitt 라이브러리 세팅

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// mitt 라이브러리 세팅
+// npm install mitt
+import mitt from 'miit'
+let emitter = mitt();
+let app = createApp(App); // app : vue 설정을 다루는 부분
+app.config.globalProperties.emitter = emitter; // 모든 컴포넌트가 mitt 라이브러리 사용하게 함
+
 createApp(App).mount('#app')
